22487299435a7ee2c67ddf9b13383cecbe8fc875
HerryCooly <592836711@qq.com>
2022-02-07 12:23:17 -0800

    Create home.php

---
+++ Login/home.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: login.html');
+	exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="login-style.css">
+  <title>Document</title>
+</head>
+<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Ethics Dashboard</h1>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+		</div>
+	</body>
+</html>
