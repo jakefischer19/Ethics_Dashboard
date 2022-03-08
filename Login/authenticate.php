1d9c4e6405bfd78019ade51b7df63952f4a263f3
Isaac <90351644+isnacc@users.noreply.github.com>
2022-03-07 17:53:25 -0800

    deleted junk code

--- Login/authenticate.php
+++ Login/authenticate.php
@@ -58,7 +58,7 @@ $_SESSION["email"] = "$email";
     $row = mysqli_fetch_row($result);
     $hash = $row[0] ?? false;
     if(password_verify($password, $hash)){
-      header("Location: https://https://herrycooly.com/EBoard/admin_page.html");
+      header("Location: https://https://herrycooly.com/EBoard/Admin/admin_page.html");
       exit;
     }
   }else{
@@ -70,50 +70,3 @@ $_SESSION["email"] = "$email";
     exit;
   }
 ?>
-
-
-/* old login code
-// Connect to database
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-
-if ( !isset($_REQUEST['email'], $_REQUEST['password']) ) {
-	// Could not get the data that should have been sent.
-	exit('Please fill both the email and password fields!');
-}
-
-if ($stmt = $con->prepare('SELECT email, password FROM students WHERE email = ?')) {
-	$stmt->bind_param('s', $_POST['email']);
-	$stmt->execute();
-	// Store the result so we can check if the account exists in the database.
-	$stmt->store_result();
-
-  if ($stmt->num_rows > 0) {
-    $stmt->bind_result($email, $password);
-    $stmt->fetch();
-    
-    // Account exists, now we verify the password.
-    // Note: remember to use password_hash in your registration file to store the hashed passwords.
-    
-    if ($_POST['password'] === $password) { 
-      
-      // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-      session_regenerate_id();
-      $_SESSION['loggedin'] = TRUE;
-      $_SESSION['name'] = $_POST['username'];
-      $_SESSION['id'] = $id;
-      header('Location: home.php');
-    } else {
-      // Incorrect password
-      echo 'Incorrect email and/or password!';
-    }
-  } else {
-    // Incorrect username
-    echo 'Incorrect email and/or password!';
-  }
-
-	$stmt->close();
-}*/
