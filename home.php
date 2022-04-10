760f35b129965cbe6445bd60e8189a1953639afe
vgarg-oc <97914925+vgarg-oc@users.noreply.github.com>
2022-04-10 11:33:37 -0700

    Revert "Username vg"

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['loggedin'])) {
 	//die(header("location: 404.php"));
-  die(header("location: login.php"));
+  die(header("location: login.html"));
 	exit;
 }
 if (!isset($_SESSION['CREATED'])) {
