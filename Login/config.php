7ca6adc3762b3fa07c745b648f5d726693a4acbc
jakefischer19 <98055934+jakefischer19@users.noreply.github.com>
2023-02-04 13:06:53 -0800

    Update config.php

--- Login/config.php
+++ Login/config.php
@@ -2,16 +2,6 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-//$DATABASE_HOST = '69.172.204.200';
-//$DATABASE_USER = 'rwalker';
-//$DATABASE_PASS = 'weN2021account';
-//$DATABASE_NAME = 'rwalker_Ethics_Dashboard_OC_224';
-
-// $DATABASE_HOST = '69.172.204.200';
-// $DATABASE_USER = 'herrycoo_yhu';
-// $DATABASE_PASS = 'hY592836711@';
-// $DATABASE_NAME = 'herrycoo_Ethic_Dashboard';
-
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
